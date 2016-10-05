8a85ab04607026441eb5189c0a72ee764505fd4d
Olivier El Mekki <olivier@el-mekki.com>
2016-10-05 18:00:33 +0200

    FIX for github

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-document.body.style.tabSize = 2;
+let tabsize = 2;
+
+document.body.style.tabSize = tabsize;
+
+// for github
+Array.prototype.forEach.call( document.querySelectorAll( '[data-tab-size]' ), function( element ){
+  element.style.tabSize = tabsize;
+});
