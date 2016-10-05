3ac377ca4f7b9de77030f7edf97944b4a812438d
Olivier El Mekki <olivier@el-mekki.com>
2016-10-05 18:05:58 +0200

    FIX for pjax

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ let tabsize = 2;
 document.body.style.tabSize = tabsize;
 
 // for github
-Array.prototype.forEach.call( document.querySelectorAll( '[data-tab-size]' ), function( element ){
-  element.style.tabSize = tabsize;
-});
+function changeTabSizes() {
+  Array.prototype.forEach.call( document.querySelectorAll( '[data-tab-size]' ), function( element ){
+    element.style.tabSize = tabsize;
+  });
+}
+changeTabSizes();
+
+// for pjax
+let treeObserver = new MutationObserver( changeTabSizes );
+treeObserver.observe( document.body, { subtree: true, childList: true });
